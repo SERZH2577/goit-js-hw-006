b247df27cbe681edaf70c55eb8fd2c2b0b7b6e44
Serhii Plis <serzh2577@gmail.com>
2023-10-27 16:53:56 +0300

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,23 @@
 class Storage {
-  getItems() {}
-  addItem(newItem) {}
-  removeItem(itemToRemove) {}
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const newArr = [];
+    for (let item of this.items) {
+      if (item !== itemToRemove) {
+        newArr.push(item);
+      }
+    }
+    return (this.items = newArr);
+  }
 }
-
-// getItems() — повертає масив поточних товарів у приватній властивості items.
-// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
-// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 
